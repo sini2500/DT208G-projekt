c68113bd9bb4a634629bff5871ae479792b50476
Simon <sini2500@student.miun.se>
2026-09-22 13:42:35 +0200

    sidor & routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Courses } from './pages/courses/courses';
+import { Schedule } from './pages/schedule/schedule';
+import { About } from './pages/about/about';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Courses },
+    { path: "kurser", component: Courses },
+    { path: "schema", component: Schedule },
+    { path: "om", component: About },
+    { path: '404', component: NotFound },
+    { path: '**', redirectTo: "404", pathMatch: "full"}
+];
